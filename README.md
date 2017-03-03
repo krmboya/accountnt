ec286c06b9227a269576b56761974606d73c2dd3
Kenny Rachuonyo <krmboya@gmail.com>
2017-03-03 13:23:56 +0300

    basics

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # accountnt
 
 An automated spending accountant
+
+## Usage
+
+1. Add a Spending Category
+
+    user: add cat professional services
+    acct: Category 'Professional Services' has been added
---
+++ accountnt.py
@@ -0,0 +1,10 @@
+from collections import namedtuple
+
+# Entities
+Category = namedtuple('Category', ['name'])
+
+# Use Cases
+
+class CategoryUsecase(object):
+    def add_category(self, name):
+        pass
