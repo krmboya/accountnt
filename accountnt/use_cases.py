5fee687c7667f06b96e5bbdceca86f1e884a0062
Kenny Rachuonyo <krmboya@gmail.com>
2017-03-10 17:31:28 +0300

    rewrite usecase as ABC

--- accountnt/use_cases.py
+++ accountnt/use_cases.py
@@ -1,23 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Use Cases for the app """
+import abc
+
 import entities
 
 
-class AddCategoryUseCase(object):
-
-    def __init__(self, category_store, user_category_store):
-        self.category_store = category_store
-        self.user_category_store = user_category_store
+class AddCategoryUseCase:
+    __metaclass__ = abc.ABCMeta
 
     def execute_with(self, category_name, user_id):
 
         category_name = entities.Category.normalized_name(category_name)
-        category, __ = self.category_store.get_or_create(category_name)
+        category = self.get_or_create(category_name)
 
-        if self.user_category_store.exists(
-                user_id, category.uid):
-            return "You've already added '{}'".format(category.name)
+        if self.user_category_exists(category_name, user_id):
+            msg = "You've already added '{}'"
+            return msg.format(category.name)
 
-        self.user_category_store.add(user_id, category.uid)
+        self.add_user_category(user_id, category.uid)
         return "Category '{}' has been added".format(category.name)
+
+    @abc.abstractmethod
+    def get_or_create(self, category_name, user_id):
+        pass
+
+    @abc.abstractmethod
+    def user_category_exists(self, category_name, user_id):
+        pass
+
+    @abc.abstractmethod
+    def add_user_category(self, category_name, user_id):
+        pass
--- tests/test_use_cases.py
+++ tests/test_use_cases.py
@@ -4,18 +4,21 @@ import unittest
 
 from mock import Mock
 
-from accountnt import use_cases as usc, entities as entt
-
-
-# mock objects
-catg_store = Mock()
-catg_store.get_or_create = Mock()
-user_catg_store = Mock()
-user_catg_store.add = Mock()
-user_catg_store.exists = Mock()
+from accountnt import use_cases as usc, entities as ent
 
 
 class AddCategoryTestCase(unittest.TestCase):
+
+    class UseCase(usc.AddCategoryUseCase):
+        pass
+
+    def setUp(self):
+
+        # create a use case subclass with mocked methods
+        self.use_case = self.__class__.UseCase()
+
+        # todo: patch relevant use case methods
+        # to make the test below work
 
     def test_category_added(self):
         """Tests that a new category is successfully added"""
@@ -23,17 +26,12 @@ class AddCategoryTestCase(unittest.TestCase):
         category_name, user_id = "motor maintenance", 1
 
         # prepare mock objects
-        catg_store.get_or_create.return_value = (
-            entt.Category(category_name, 2),
-            True)
+        self.user_case.get_or_create.return_value = ent.Category(
+            category_name, 2)
 
-        user_catg_store.exists.return_value = False
+        # execute use case
+        self.user_case.execute_with("motor maintenance", user_id)
 
-        add_catg = usc.AddCategoryUseCase(
-            catg_store, user_catg_store)
-
-        add_catg.execute_with("motor maintenance", user_id)
-
-        catg_store.get_or_create.assert_called_with("Motor Maintenance")
-
-        user_catg_store.add.assert_called_with(user_id, 2)
+        # assert methods
+        self.use_case.get_or_create.assert_called_with("Motor Maintenance")
+        self.use_case.add.assert_called_with(1, 2)
