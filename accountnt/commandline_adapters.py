f214fc870fb5a6e5c821e2bbb4d918dd17aacefb
Kenny Rachuonyo <krmboya@gmail.com>
2017-03-11 14:11:34 +0300

    add commandline adapter + test

---
+++ accountnt/commandline_adapters.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Adapt use cases for the filesystem"""
+import io
+import os
+
+import entities
+import use_cases
+
+
+def ensure_dir(dirname):
+    """Create directory dirname if it doesn't exist"""
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+
+
+def ensure_file(filename):
+    """Create file `filename` if it doesn't exist"""
+    with open(filename, 'a'):
+        os.utime(filename, None)
+
+
+class AddCategory(use_cases.AddCategoryUseCase):
+
+    def __init__(self, storage_directory):
+
+        self._categories_filename = "categories.txt"
+        self._user_categories_filename = "ucategories.txt"
+
+        # Ensure the directory exists
+        ensure_dir(storage_directory)
+
+        # set full path to storage files
+        self.categories_file = os.path.join(
+            storage_directory,
+            self._categories_filename)
+
+        self.user_categories_file = os.path.join(
+            storage_directory,
+            self._user_categories_filename)
+
+        # ensure they exists
+        ensure_file(self.categories_file)
+        ensure_file(self.user_categories_file)
+
+    def get_or_create(self, category_name):
+
+        with io.open(self.categories_file, 'r+t') as f:
+
+            for i, line in enumerate(f):
+                if line == category_name:
+                    return entities.Category(category_name, i)
+
+            f.write(category_name + u"\n")
+            f.flush()
+            return entities.Category(category_name, i+1)
+
+    def user_category_exists(self, user_id, category_id):
+
+        with io.open(self.user_categories_file) as f:
+            matching_category = [line for line in f
+                                 if line.strip() == str(category_id)]
+
+            return len(matching_category) > 0
+
+    def add_user_category(self, user_id, category_id):
+
+        with io.open(self.user_categories_file, 'at') as f:
+            f.write(unicode(category_id) + "\n")
---
+++ tests/test_commandline_adapters.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+import io
+import tempfile
+import os
+
+from accountnt import (
+    commandline_adapters as cmd_adapters, entities
+)
+
+
+class AddCategoryFileSystemTestCase(unittest.TestCase):
+
+    def test_can_add_user_category(self):
+
+        storage_dir = os.path.join(
+            tempfile.gettempdir(), ".accountnt")
+        category_name = 'professional services'
+
+        use_case = cmd_adapters.AddCategory(storage_dir)
+
+        use_case.execute_with(category_name, 1001)
+        normalized_name = entities.Category.normalized_name(category_name)
+        with io.open(use_case.categories_file) as f:
+            matching_lines = [line for line in f
+                              if line.strip() == normalized_name]
+
+        self.assertTrue(len(matching_lines) > 0)
