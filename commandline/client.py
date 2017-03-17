b4aafb7da65b51824b178f20f1d113e7c82a4d72
Kenny Rachuonyo <krmboya@gmail.com>
2017-03-17 16:29:00 +0300

    rename client module, fix bugs + test

--- commandline/client.py
+++ commandline/client.py
@@ -28,6 +28,7 @@ def execute_command(*args):
     """Given the user arguments, invoke the appropriate command"""
 
     command_name, command_args = args[0], args[1:]
+
     try:
         command_handler = handlers.get(command_name)
     except handlers.CommandNotFound:
@@ -40,7 +41,8 @@ def execute_command(*args):
 if __name__ == "__main__":
     args = sys.argv
 
-    if len(args) > 2:
+    if len(args) < 2:
         exit_with_error(usage)
 
-    execute_command(args)
+    response = execute_command(*args[1:])
+    sys.stdout.write(response + "\n")
--- tests/test_commandline_client.py
+++ tests/test_commandline_client.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import unittest
+
+from commandline import client
+
+
+class ClientTestCase(unittest.TestCase):
+
+    def test_unknown_command_msg(self):
+
+        command = "foobar"
+
+        response = client.execute_command(*[command])
+
+        self.assertTrue(client.unknown_command_msg in response)
+        self.assertTrue(client.usage in response)
